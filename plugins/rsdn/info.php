cf02c6a453fdffb8d97f241a9ee3385fd910ee0a
bubu <bubu@riseup.net>
2022-09-16 17:06:44 +0200

    Ajout plugin rsdn : première déclaration

---
+++ plugins/rsdn/info.php
@@ -0,0 +1,8 @@
+<?php return array(
+        "name"        => "rsdn",            // plugin name
+        "version"     => "1",                     // plugin version
+        "author"      => "bubu",           // author name
+        "website"     => "https://feng.sortirdunucleaire.org", // author website URL
+        "description" => "Avoir des rapports pratique !"   // brief description	
+    );?>
+
